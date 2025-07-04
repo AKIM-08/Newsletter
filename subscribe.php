da8da4ee5b85d1e4f6768464c8c2a7573b6537e3
AKIM-08 <marcivan313@gmail.com>
2025-07-04 00:42:06 -0400

    script pour la vérification des informations et l'ajout à la BD

--- subscribe.php
+++ subscribe.php
@@ -15,10 +15,11 @@
 
 
 <?php 
+require 'config/db.php';
 $postData = $_POST;
 $error = null;
 $success = null;
-
+// vérification de la validité des champs remplis : nom, prenom, email, numéro
 if (
     !isset($postData['email'])
     || !filter_var($postData['email'], FILTER_VALIDATE_EMAIL)
@@ -30,16 +31,49 @@ if (
     || trim($postData['numero']) === ''
     || trim(!preg_match("/^\+\d{1,4} \d{6,15}$/", $postData['numero']))
 ) {
+    // message d'erreur en cas de format de numéro non respecter ou email non valide
     $error = 'Il faut un numéro et une addresse email valide pour s\'inscrire !';
 } else {
-    $success = 'Inscription validée !';
-}
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $prenom = $_POST['prenom'];
+        $nom = $_POST['nom'];
+        $number = $_POST['numero'];
+        $email = $_POST['email'];
+        // verification de l'email pour voir s'il n'existe pas déjà, $verifie va vérifier si l'email existe après l'avoir use avec execute() et fetch() sert à récupérer les résultats de cette action
+            if (!empty($prenom) && !empty($nom) && !empty($number) && !empty($email)) {
+                $sql_vérifie = 'SELECT email FROM users where email = :email';
+                $verifie = $pdo->prepare($sql_vérifie);
+                $verifie->execute(['email' => $_POST['email']]);
+                if ($verifie->fetch()) {
+                        $error2 = 'Cet email est déjà lié à un compte, veuillez réessayer !';
+                    } else {
+                        // dans le cas où l'email est valide et n'existe pas on insère les données dans la bd 
+                        $requete = $pdo->prepare('INSERT INTO users(first_name, name, number, email) VALUE (:first_name, :name, :number, :email)');
+                        $requete->bindvalue(':first_name', $prenom);
+                        $requete->bindvalue(':name', $nom);
+                        $requete->bindvalue(':number', $number);
+                        $requete->bindvalue(':email', $email);
+                        $requete->execute();
+                        $success = 'Inscription validée !';
+                    }
+                }
+            }
+    }
 ?>
 
-
+// affichage des messages 
 <?php if ($error) : ?>
     <div class="alert alert-danger">
         <?= $error ?>
+    </div>
+    <div class="d-flex justify-content-center">
+        <a class="btn btn-primary text-center" href="/index.php" role="button">Reprendre</a>
+    </div>
+<?php endif ?>
+
+<?php if ($error2) : ?>
+    <div class="alert alert-danger">
+        <?= $error2 ?>
     </div>
     <div class="d-flex justify-content-center">
         <a class="btn btn-primary text-center" href="/index.php" role="button">Reprendre</a>
